59ec56bde214e46840f5b51e2ec32f68b73246b8
taka-fukase <40255403+taka-fukase@users.noreply.github.com>
2023-04-13 23:18:24 +0900

    B06修正

--- Tessoku/B06.py
+++ Tessoku/B06.py
@@ -1,4 +1,4 @@
-import numpy as np
+import itertools
 
 # 入力
 N = int(input())
@@ -11,14 +11,18 @@ for i in range(Q):
 	
 
 # 累積和の配列を作成
-cumulative_sum = np.cumsum(A)
+cumulative_sum = list(itertools.accumulate(A))
 
 # 各問題を解く
 for i in range(Q):
-	rate = (cumulative_sum[R[i]] - cumulative_sum[L[i]]) / (R[i] - L[i])
-	if rate == 0.5:
-		print("draw")
-	elif rate < 0.5:
+	if L[i] == 1:
+		rate = cumulative_sum[R[i]-1]/R[i]
+	else:
+	    rate = (cumulative_sum[R[i]-1] - cumulative_sum[L[i]-2]) / (R[i] - L[i] + 1)
+	
+	if rate < 0.5:
 		print("lose")
 	elif rate > 0.5:
 		print("win")
+	elif rate == 0.5:
+		print("draw")
