58310db6f2a7b98758e90d8c4cd58cc729facf83
taka-fukase <40255403+taka-fukase@users.noreply.github.com>
2023-04-15 11:15:41 +0900

    B09修正中

--- Tessoku/B09.py
+++ Tessoku/B09.py
@@ -1,9 +1,5 @@
 # 入力
 N = int(input())
-A = [None]*N
-B = [None]*N
-C = [None]*N
-D = [None]*N
 count = 0
 
 matrix = [[0]*1501 for _ in range(1501)]
@@ -15,12 +11,12 @@ for _ in range(N):
     matrix[D][C] = matrix[D][C] + 1
 
 # 横に走査
-for y in range(1, 1501):
-    for x in range(1, 1501):
+for y in range(0, 1500):
+    for x in range(0, 1500):
       matrix[y][x] = matrix[y][x-1] + matrix[y][x]
 # 縦に走査
-for y in range(1, 1501):
-    for x in range(1, 1501):
+for y in range(0, 1500):
+    for x in range(0, 1500):
         matrix[y][x] = matrix[y-1][x] + matrix[y][x]
         if matrix[y][x] > 0:
             count = count + 1
