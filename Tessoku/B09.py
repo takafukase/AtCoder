065311b3d730dcf3376fb4230e19ceddef02d029
taka-fukase <40255403+taka-fukase@users.noreply.github.com>
2023-04-15 11:32:46 +0900

    B09実装

--- Tessoku/B09.py
+++ Tessoku/B09.py
@@ -1,22 +1,24 @@
 # 入力
 N = int(input())
 count = 0
+matrix_size = 1502
 
-matrix = [[0]*1501 for _ in range(1501)]
+matrix = [[0]*matrix_size for _ in range(matrix_size)]
 for _ in range(N):
     A, B, C, D = map(int, input().split())
-    matrix[B][A] = matrix[B][A] + 1
-    matrix[B][C] = matrix[B][C] - 1
-    matrix[D][A] = matrix[D][A] - 1
-    matrix[D][C] = matrix[D][C] + 1
+    # 走査するときに0始まりなので、+1シフトしておく
+    matrix[B+1][A+1] = matrix[B+1][A+1] + 1
+    matrix[B+1][C+1] = matrix[B+1][C+1] - 1
+    matrix[D+1][A+1] = matrix[D+1][A+1] - 1
+    matrix[D+1][C+1] = matrix[D+1][C+1] + 1
 
 # 横に走査
-for y in range(0, 1500):
-    for x in range(0, 1500):
+for y in range(1, matrix_size):
+    for x in range(1, matrix_size):
       matrix[y][x] = matrix[y][x-1] + matrix[y][x]
 # 縦に走査
-for y in range(0, 1500):
-    for x in range(0, 1500):
+for y in range(1, matrix_size):
+    for x in range(1, matrix_size):
         matrix[y][x] = matrix[y-1][x] + matrix[y][x]
         if matrix[y][x] > 0:
             count = count + 1
