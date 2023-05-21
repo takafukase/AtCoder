92ed1376b6d91ae443dc35d20c72a69f95e0e1e1
taka-fukase <40255403+taka-fukase@users.noreply.github.com>
2023-05-22 01:18:51 +0900

    B13しゃくとり法に修正

--- Tessoku/3. 2分探索/B13.py
+++ Tessoku/3. 2分探索/B13.py
@@ -5,38 +5,27 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 A.insert(0, 0)
 
-# 2分探索
-def search(A, I):
+def two_pointer_technique(A, x):
     """
-    配列AのI番目以降の要素に対して、function()の2分探索を行う
+    尺取り法
+    ソートされた配列Aに対して、条件（差分がx以下になる）を満たす組み合わせを探索する
     """
-    L = I + 1
-    R = len(A) - 1
-    while(L <= R):
-        C = (L + R) // 2
-        value = function(A, I, C)
-        if K == value:
-            return C - I
-        elif K < value:
-            R = C - 1
-        elif K > value:
-            L = C + 1
-    return L - I - 1
-
-def function(A, I, X):
-    """
-    A: 累積和配列
-    I番目からX番目のI番目までの累積和を計算する
-    """
-    ans = A[X] - A[I]
+    ans = 0
+    R = 1
+    for i in range(1, len(A)):
+        for j in range(R, len(A)):
+            if A[j] - A[i-1] > x:
+                ans += j - i
+                R = j - 1
+                break
+            elif j == len(A) - 1:
+                ans += j - i + 1
+                R =j
     return ans
 
 if __name__ == "__main__":
     # 累積和を求める
     cumsum = list(itertools.accumulate(A))
-
-    ans = 0
-    for i in range(N):
-        ans = ans + search(cumsum, i)
+    ans = two_pointer_technique(cumsum, K)
     print(ans)
 
