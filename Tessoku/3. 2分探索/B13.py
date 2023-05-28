0600e56ded12c6a68330a88bf1de014eaf4d2c93
taka-fukase <40255403+taka-fukase@users.noreply.github.com>
2023-05-28 13:17:32 +0900

    B13 while分に変更、コメント追加

--- Tessoku/3. 2分探索/B13.py
+++ Tessoku/3. 2分探索/B13.py
@@ -13,14 +13,11 @@ def two_pointer_technique(A, x):
     ans = 0
     R = 1
     for i in range(1, len(A)):
-        for j in range(R, len(A)):
-            if A[j] - A[i-1] > x:
-                ans += j - i
-                R = j - 1
-                break
-            elif j == len(A) - 1:
-                ans += j - i + 1
-                R =j
+        j = R
+        while j < len(A) and A[j] - A[i-1] <= x:
+            j += 1
+        ans += j - i
+        R = j - 1
     return ans
 
 if __name__ == "__main__":
@@ -29,3 +26,21 @@ if __name__ == "__main__":
     ans = two_pointer_technique(cumsum, K)
     print(ans)
 
+
+"""
+テストケース
+N, K : 7 50
+A : (0) 11 12 16 22 27 28 31
+累積和cumsum : 0, 11, 23, 39, 61, 88, 116, 147
+
+考察1
+たとえば、3個目から5個目までの和は、
+A[3] + A[4] + A[5] = 16 + 22 + 27 = 65
+cumsum[5] - cumsum[2] = 65
+一般化すると、i番目からj番目までの和は,cumsum[j] - cumsum[i-1]
+
+考察2
+たとえば、i = 1のとき、j = 1, 2, 3のとき要素の総和はK(=50)以下である。このとき、小さい要素のインデックスを1とする総和が50以下の組み合わせは、3個ある。
+
+一般化すると、i番目からj番目までの要素の和はK以下で、i番目からj+1番目までの要素の和はKより大きかった。このとき、小さい要素のインデックスをiとする総和がK以下の組み合わせは、(j + 1) - i個ある。
+"""
