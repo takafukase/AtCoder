dc5564d133cd7e307f4df5f442a393cfaf9b8b70
taka-fukase <40255403+taka-fukase@users.noreply.github.com>
2023-06-23 20:32:56 +0900

    A19実装

---
+++ Tessoku/4. 動的計画/A19.py
@@ -0,0 +1,24 @@
+# input
+N, S = map(int, input().split())
+# W = []
+# V = []
+# for i in range(N):
+#     tmp = list(map(int, input().split()))
+#     W.append(tmp[0])
+#     V.append(tmp[1])
+W, V = zip(*[map(int, input().split()) for _ in range(N)])
+
+if __name__ == "__main__":
+    ## 動的計画法
+    dp = [[-1]*(S+1) for _ in range(N+1)]
+    dp[0][0] = 0
+
+    for i in range(N):
+        for j in range(S+1):
+            if dp[i][j] >= 0:
+                if 
+                dp[i+1][j] = max(dp[i+1][j], dp[i][j])
+                if j + W[i] <= S:
+                    dp[i+1][j+W[i]] = max(dp[i+1][j+W[i]], dp[i][j] + V[i])
+    
+    print(max(dp[-1]))
